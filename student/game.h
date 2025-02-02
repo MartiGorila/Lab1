e19b2f2c1c7376d288c02d7ae2191de4bc637502
MartiGorila <marti.delmoral01@estudiant.upf.edu>
2025-02-02 12:02:04 +0100

    Fixes

--- student/game.h
+++ student/game.h
@@ -15,10 +15,10 @@ typedef struct {
   int at_col;
 } PieceInfo;
 
-typedef struct {
+typedef struct GameState {
   int score;
   char board[MAX_ROWS][MAX_COLUMNS];
-  PieceInfo curent_piece;
+  PieceInfo current_piece;
 
   // ToDo in LAB 1 - define data structure DONE
 } GameState;
@@ -35,14 +35,16 @@ bool is_collision(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info);
 
 /**** LAB 1 - functions to program (start here) ****/
 void init_game_state(GameState *game_state);
-  
-bool is_terminal(char board[MAX_ROWS][MAX_COLUMNS]); // True if contains X
-  
-                                                      // values in TOP-4 rows
-void move_piece(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option);
-  
-void rotate(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option);
-  
+
+bool is_terminal(char board[MAX_ROWS][MAX_COLUMNS]);  // True if contains X
+
+// values in TOP-4 rows
+void move_piece(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info,
+                int option);
+
+void rotate(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info,
+            int option);
+
 /**** LAB 1 - functions to program (end here) ****/
 
 /// Implemented functions
